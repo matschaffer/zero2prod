0bd5ae83ae3062776bf51728a8f3fddf18f50a37
Mat Schaffer <mat@elastic.co>
2022-07-12 13:08:25 +0000

    Add output for auto-forwarding

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use zero2prod::run;
 async fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8080";
     let listener = std::net::TcpListener::bind(addr);
+    println!("Listening on {}", addr);
     run(listener?)?.await
 }
