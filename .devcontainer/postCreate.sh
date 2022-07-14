4f5e74127c4c6a37d4d8ec8f4416b1106fd2a5ef
Mat Schaffer <mat@elastic.co>
2022-07-14 15:07:49 +0000

    Fix pgpass perms

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,5 +3,6 @@
 cat > ~/.pgpass <<EOF
 ${PGHOST}:${PGPORT}:${PGDATABASE}:${PGUSER}:${POSTGRES_PASSWORD}
 EOF
+chmod 600 ~/.pgpass
 
 cargo install cargo-edit cargo-watch cargo-tarpaulin cargo-expand
