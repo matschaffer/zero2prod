c8d1dc8eaa99d6afb679644cee488845e81a33a3
Mat Schaffer <mat@elastic.co>
2022-07-14 23:09:35 +0000

    Add nightly and cargo volume

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,4 +5,5 @@ ${PGHOST}:${PGPORT}:${PGDATABASE}:${PGUSER}:${POSTGRES_PASSWORD}
 EOF
 chmod 600 ~/.pgpass
 
+sudo chown vscode:vscode /usr/local/cargo/registry
 cargo install cargo-edit cargo-watch cargo-tarpaulin cargo-expand
