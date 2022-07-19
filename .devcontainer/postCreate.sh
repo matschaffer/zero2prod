e2ad6aabc5567d439a60ee5171b77be3bc75e37d
Mat Schaffer <mat@elastic.co>
2022-07-19 08:17:19 +0000

    Migrate as part of rebuild

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,3 +9,5 @@ sudo chown vscode:vscode /usr/local/cargo/registry
 cargo install cargo-edit cargo-watch cargo-tarpaulin cargo-expand
 
 cargo install --version=0.6.0 sqlx-cli --no-default-features --features rustls,postgres
+
+sqlx migrate run
