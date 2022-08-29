4e3b27aee4e2fb305305112ddaf8ff8b2279adfb
Mat Schaffer <mat@elastic.co>
2022-08-29 03:32:05 +0000

    Add bunyan and udeps

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,8 +6,10 @@ EOF
 chmod 600 ~/.pgpass
 
 sudo chown vscode:vscode /usr/local/cargo/registry
-cargo install cargo-edit cargo-watch cargo-tarpaulin cargo-expand cargo-audit
+cargo install cargo-edit cargo-watch cargo-tarpaulin cargo-expand cargo-audit cargo-udeps
 
 cargo install --version=0.6.0 sqlx-cli --no-default-features --features rustls,postgres
+
+cargo install bunyan
 
 sqlx migrate run
