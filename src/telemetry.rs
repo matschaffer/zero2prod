6ce980f7065709cb7f4c92a9fec34126fce6749a
Mat Schaffer <mat@elastic.co>
2022-08-29 05:26:21 +0000

    Unused import

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,4 +1,3 @@
-use tokio::io::Sink;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
